7d77b5b3253ac7f3e2c5be2b0a1eed953fe2f3aa
alejandrac11 <acuesta67588@umanizales.edu.co>
2021-04-19 23:14:19 -0500

    Homework

---
+++ src/pages/products/components/product-row/products-rowFour.comp.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './product-rowRed.style.css';
+
+const ProductRowFour = () => {
+    return (
+        <div className="product-rowRed-container">
+           iPhone 5    $399.99
+        </div>
+        
+    );
+}
+
+export default ProductRowFour;
---
+++ src/pages/products/components/product-row/products-rowThree.comp.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './product-row.style.css';
+
+const ProductRowThree = () => {
+    return (
+        <div className="product-row-container">
+           iPod Touch    $99.99
+        </div>
+        
+    );
+}
+
+export default ProductRowThree;
